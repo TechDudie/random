5d5e740e21202f6e1fb51c0f0263da7f865b5fd8
TechnoDotΩ <73961295+TechDudie@users.noreply.github.com>
2022-04-15 17:41:03 -0500

    Update main.min.py

--- main.min.py
+++ main.min.py
@@ -1,36 +1,33 @@
-import random
+import random as O
 # a = 38563919364836273485827934683747283748362946584658264857384729485629475629485636287498385638294658374728019182084730719384659381628749484628637352761838452629573518385638293846338563936593846295758365
-e=int
-f=range
-p=print
-h="This number is "
-j="random."
-k=f"not {j}"
-a=random.randint(e("1"+"0"*199),e("9"*200))
-a=str(a)
-a=''.join([g*2 for g in a])
-a=a[1:-1]
-a=[a[i:i + 2] for i in f(0,len(a),2)]
-x=0
-for i in a:
-    a[x]=e(i)
-    x+=1
-b=[]
-for i in f(0,100):b.append(0)
-for i in a:b[i-1]+=1
-c=0
-for i in b:
-    if i>5:c+=i-5
-z=0
-o=0
-for i in b:
-    if i==0:z+=1
-    if i==1:o+=1
-d=0
-if c>=7:d+=1
-if z>=17:d+=1
-if o<=20:d+=1
-if d>=2:p(f"{h}{k}")
-else:
-    if c>=7:p(f"{h}probably {k}")
-    else:p(f"{h}{j}")
+E=int
+I=range
+F=print
+G='This number is '
+J='random.'
+K=f"not {J}"
+A=O.randint(E('1'+'0'*199),E('9'*200))
+A=str(A)
+A=''.join([B*2 for B in A])
+A=A[1:-1]
+A=[A[B:B+2]for B in I(0,len(A),2)]
+L=0
+for B in A:A[L]=E(B);L+=1
+C=[]
+for B in I(0,100):C.append(0)
+for B in A:C[B-1]+=1
+H=0
+for B in C:
+	if B>5:H+=B-5
+M=0
+N=0
+for B in C:
+	if B==0:M+=1
+	if B==1:N+=1
+D=0
+if H>=7:D+=1
+if M>=17:D+=1
+if N<=20:D+=1
+if D>=2:F(f"{G}{K}")
+elif H>=7:F(f"{G}probably {K}")
+else:F(f"{G}{J}")
